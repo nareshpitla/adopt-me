fdfd4b783011a3dfb5915f4d057b9f4d34f82b49
Naresh Pitla <nareshnanimudiraj@gmail.com>
2022-03-23 23:32:13 +0530

    home laptop 23-03-2022

--- src/App.js
+++ src/App.js
@@ -5,24 +5,14 @@ import Pet from "./Pet";
 
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {id: "my-brand"}, "Adopt Me!"),
-    React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-        name: "Beam",
-        animal: "Dog",
-        breed: "Wheaten Terrier",
-    }),
-  ]);
-};
+  return (
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Luna" animal="Dog" breed="havanese" />
+    <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+    <Pet name="Beam" animal="Dog" breed="Wheaten Terrier" />
+  </div>
+  );
+}
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
