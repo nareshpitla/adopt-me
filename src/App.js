e3981cd7b32d09dc35313f15cb4a32fbb317402f
Naresh Pitla <nareshnanimudiraj@gmail.com>
2022-03-23 19:49:23 +0530

    initial commit
    initial commint

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+import Pet from "./Pet";
+ 
+
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {id: "my-brand"}, "Adopt Me!"),
+    React.createElement(Pet, {
+        name: "Luna",
+        animal: "Dog",
+        breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+        name: "Pepper",
+        animal: "Bird",
+        breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+        name: "Beam",
+        animal: "Dog",
+        breed: "Wheaten Terrier",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
